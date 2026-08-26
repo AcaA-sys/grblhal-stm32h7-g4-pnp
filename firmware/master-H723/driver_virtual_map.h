c3206570db797c9d89945faccb3786d11e842100
AcaA-sys <titanik71@yandex.com>
2026-08-26 14:45:22 +0300

    Update and rename master-H723.ioc to driver_virtual_map.h

---
+++ firmware/master-H723/driver_virtual_map.h
@@ -0,0 +1,188 @@
+/*
+  driver_virtual_map.h - Карта пинов для Full-Digital режима Мастера Nexus-H7-FD
+  Все оси моторов назначены ВИРТУАЛЬНЫМИ (GPIO_NUM_NC). 
+  Физические разъемы двигателей на плате отсутствуют.
+ * MCU: STM32H723VGT6 (550MHz)
+ * Target: grblHAL / OpenPnP
+ * 
+ * Автор: AcaA-sys
+*/
+
+#ifndef _DRIVER_VIRTUAL_MAP_H_
+#define _DRIVER_VIRTUAL_MAP_H_
+
+
+// 1. ТАКТИРОВАНИЕ ( 25МГц)
+#define HSE_VALUE 25000000UL
+
+// 1. КОНФИГУРАЦИЯ КОЛИЧЕСТВА ОСЕЙ
+// Объявляем стандартные 8 оси для OpenPnP (X, Y, Z и ось вращения сопла C)
+#define N_AXIS 8
+
+// 2. ВИРТУАЛИЗАЦИЯ ОСЕЙ ДВИЖЕНИЯ (Step / Dir)
+// Назначаем константу GPIO_NUM_NC. Кремниевые выводы процессора остаются свободными.
+#define STEP_PORT       NULL
+#define DIR_PORT        NULL
+
+#define X_STEP_PIN      GPIO_NUM_NC
+#define X_DIR_PIN       GPIO_NUM_NC
+
+#define Y_STEP_PIN      GPIO_NUM_NC
+#define Y_DIR_PIN       GPIO_NUM_NC
+
+#define Z_STEP_PIN      GPIO_NUM_NC  // Z1_STEP
+#define Z_DIR_PIN       GPIO_NUM_NC
+
+#define A_STEP_PIN      GPIO_NUM_NC  // Z2_STEP
+#define A_DIR_PIN       GPIO_NUM_NC
+
+#define B_STEP_PIN      GPIO_NUM_NC  // Z3_STEP
+#define B_DIR_PIN       GPIO_NUM_NC
+
+#define C_STEP_PIN      GPIO_NUM_NC  // Z4_STEP
+#define C_DIR_PIN       GPIO_NUM_NC
+
+#define U_STEP_PIN      GPIO_NUM_NC  // ось C (Rotation)
+#define U_DIR_PIN       GPIO_NUM_NC
+
+#define W_STEP_PIN      GPIO_NUM_NC  // ось C (Rotation)
+#define W_DIR_PIN       GPIO_NUM_NC
+
+#define P_STEP_PIN      GPIO_NUM_NC  // ось C (Rotation)
+#define P_DIR_PIN       GPIO_NUM_NC
+
+#define Q_STEP_PIN      GPIO_NUM_NC  // ось C (Rotation)
+#define Q_DIR_PIN       GPIO_NUM_NC
+
+#define V_STEP_PIN      GPIO_NUM_NC  // ось C (Rotation)
+#define V_DIR_PIN       GPIO_NUM_NC
+
+#define R_STEP_PIN      GPIO_NUM_NC  // ось C (Rotation)
+#define R_DIR_PIN       GPIO_NUM_NC
+
+#define D_STEP_PIN      GPIO_NUM_NC  // ось C (Rotation)
+#define D_DIR_PIN       GPIO_NUM_NC
+
+#define E_STEP_PIN      GPIO_NUM_NC  // ось C (Rotation)
+#define E_DIR_PIN       GPIO_NUM_NC
+
+// 3. ВИРТУАЛИЗАЦИЯ СИГНАЛОВ УПРАВЛЕНИЯ ДРАЙВЕРАМИ (Enable)
+// Сигналы EN также уходят в виртуальную память и будут передаваться цифровым флагом по SPI2
+#define X_ENABLE_PIN    GPIO_NUM_NC
+#define Y_ENABLE_PIN    GPIO_NUM_NC
+#define Z_ENABLE_PIN    GPIO_NUM_NC
+#define A_ENABLE_PIN    GPIO_NUM_NC
+#define B_ENABLE_PIN    GPIO_NUM_NC
+#define C_ENABLE_PIN    GPIO_NUM_NC
+#define U_ENABLE_PIN    GPIO_NUM_NC
+#define W_ENABLE_PIN    GPIO_NUM_NC
+#define P_ENABLE_PIN    GPIO_NUM_NC
+#define Q_ENABLE_PIN    GPIO_NUM_NC
+#define V_ENABLE_PIN    GPIO_NUM_NC
+#define R_ENABLE_PIN    GPIO_NUM_NC
+#define D_ENABLE_PIN    GPIO_NUM_NC
+#define E_ENABLE_PIN    GPIO_NUM_NC
+
+// 4. ФИЗИЧЕСКИЙ МЕЖЧИПОВЫЙ ИНТЕРФЕЙС SPI2_ ДЛЯ СВЯЗИ С СОПРОЦЕССОРОМ NGW-FD
+// Эти пины жестко разводятся на плате Nexus-H7-FD короткими дорожками до STM32G474
+#define SPI2_PORT       GPIOB
+#define SPI2_NSS_PIN    PIN_12   // PB12 -> SPI2_NSS (Hardware Chip Select Slave) )
+#define SPI2_SCK_PIN    PIN_13   // PB13 -> SPI2_SCK (Master Clock)
+#define SPI2_MISO_PIN   PIN_14   // PB14 -> SPI2_MISO (Master Input / Slave Output)
+#define SPI2_MOSI_PIN   PIN_15   // PB15 -> SPI2_MOSI (Master Output / Slave Input)
+#define PORT       GPIOD
+#define READY    PIN_3   // GPIO_EXTI3 PD3 <- READY (Master )
+#define PORT       GPIOA
+#define OSC_PIN   PIN_8   //   настроить как RCC_MCO1 (выход тактовой частоты 25 МГц для хаба G474)
+
+
+
+// 5. ФИЗИЧЕСКАЯ ЛИНИЯ АППАРАТНОЙ АВАРИИ (Hardware Interlock)
+// Единственный силовой провод безопасности. Сюда заводится выход HW_E-STOP от Сопроцессора
+#define HARDWARE_RESET_PORT  GPIOA
+#define HARDWARE_RESET_PIN   PIN_0   // GPIO_EXTI PA0 -> Жесткое EXTI прерывание сброса планировщика ЧПУ
+
+
+
+// 6. ФИЗИЧЕСКИЕ КОНЦЕВИКИ СТАНИНЫ (X и Y)
+// Концевики портала остаются физическими на плате Мастера для безопасного хоуминга базы
+//#define X_LIMIT_PORT    GPIOA
+//#define X_LIMIT_PIN     PIN_1    // GPIO_EXTI PA1 -> Физический концевик оси X
+#define Y_LIMIT_PORT    GPIOC  //GPIO_EXTI
+#define Y_LIMIT_PIN     PIN_4    // PC4 -> Физический концевик оси Y
+//#define **_LIMIT_PORT    GPIOB
+//#define **_LIMIT_PIN     PIN_2    // GPIO_EXTI PB2 -> **
+
+
+
+// 7. Концевик оси Z на Мастере отсутствует (концевики сопел физически опрашиваются на башне по FDCAN2)
+#define X_LIMIT_PIN     GPIO_NUM_NC
+#define Z1_LIMIT_PIN     GPIO_NUM_NC
+#define Z2_LIMIT_PIN     GPIO_NUM_NC
+#define Z3_LIMIT_PIN     GPIO_NUM_NC
+#define Z4_LIMIT_PIN     GPIO_NUM_NC
+
+#endif /* _DRIVER_VIRTUAL_MAP_H_ */
+-------------------------------------------------
+// 8. Блокировка драйверов (Безопасность)
+#define UCC_DIS_PORT GPIOB
+#define UCC_DIS_PIN 10 // PB10 (Высокий уровень = Отключено)
+
+// 9. СВЯЗЬ И ИНТЕРФЕЙСЫ
+// RS-485 (USART2) NSIP93086HV-DSWR
+#define RS485_PORT GPIOD
+#define RS485_TX_PIN 5 // PD5
+#define RS485_RX_PIN 6 // PD6
+#define RS485_DE_PIN 4 // PD4 (управление направлением)
+
+// 9.1 (USART3)
+#define HMI_PORT GPIOD
+#define HMI_TX_PIN 8 // PD8
+#define HMI_RX_PIN 9 // PD9
+
+// 10. АНАЛОГОВЫЕ ВХОДЫ (ADC)
+#define ADC_PORT GPIOA
+#define VACUUM1_PIN 2 // PA2 (ADC1_IN14) / XGZP6847  10K/20K
+#define VACUUM2_PIN 3 // PA3 (ADC1_IN15) / XGZP6847  10K/20K
+//#define ADC_PORT GPIOC
+//#define VACUUM3_PIN 3 // PC0 (ADC) / XGZP6847  10K/20K
+//#define VACUUM4_PIN 3 // PC1 (ADC) / XGZP6847  10K/20K
+#define VBUS_DET_PIN 9 // PA9 (USB Sense) / 10K/10K
+// 10.1 MPG
+#define JOY_PORT GPIOB
+#define JOY_SCALE_PIN 0 // PB0 (ADC1_IN18) Аналоговый статус галетника множителей / 5.1K/10K
+#define JOY_AXIS_PIN 1 // PB1 (ADC1_IN19)  Аналоговый статус галетника осей / 5.1K/10K
+
+
+#define JOY_PORT GPIOE
+#define JOY_А_PIN 6 // PE9 ──► Аппаратный вход TIM1_CH1 (Фаза А маховика MPG) uA9639
+#define JOY_B_PIN 6 // PE11 ──► Аппаратный вход TIM1_CH2 (Фаза B маховика MPG) uA9639
+
+
+// 11. ПАМЯТЬ 24LC16B/FM24CL16B
+#define I2C1_PORT GPIOB
+#define I2C1_SCL_PIN 8 // PB8
+#define I2C1_SDA_PIN 9 // PB9
+#define I2C1_WP_PIN 7 // PB7
+
+// 12. СИЛОВАЯ ПЕРИФЕРИЯ (PWM) - Порт E / UCC21520DW + NTMFS5C628
+#define AUX_PWM_PORT GPIOE
+#define OUT1_PIN 12 // PE12 (TIM1_CH3N)
+#define OUT2_PIN 13 // PE13 (TIM1_CH3)
+#define OUT3_PIN 14 // PE14 (TIM1_CH4)
+#define OUT4_PIN 15 // PE15 (TIM1_BKIN2)
+
+//#define AUXOUTPUT5_PORT GPIOE // оптрон АТС / 74LVC2G07 + TLP291
+//#define AUXOUTPUT5_PIN 5
+//#define AUXOUTPUT6_PORT GPIOE // оптрон АТС / 74LVC2G07 + TLP291
+//#define AUXOUTPUT6_PIN 6
+
+//#define LED_PORT GPIOC    // 74LVC2G07
+//#define STATUS_LED_PIN 2 // PC2
+//#define ERROR_LED_PIN 3 // PC3
+
+// 13. СПЕЦИАЛЬНЫЕ НАСТРОЙКИ (grblHAL)
+// Отключение детекции VBUS на PA9 
+#define USB_VBUS_DETECTION_DISABLED
+
+#endif
