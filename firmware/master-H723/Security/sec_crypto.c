3002832d29f86a01100a7156af71f92ace8b0a76
AcaA-sys <titanik71@yandex.com>
2026-08-26 05:15:47 +0300

    Update sec_crypto.c
    Внутри ваших файлов sec_crypto.c и pnp_hub_plugin.c вся тяжелая логика проверок, затирания Flash-памяти и карусельного искажения координат теперь заворачивается в стандартные директивы препроцессора #if / #endif.

--- firmware/master-H723/Security/sec_crypto.c
+++ firmware/master-H723/Security/sec_crypto.c
@@ -1,1 +1,15 @@
+// Внутри файла sec_crypto.c
+void check_hardware_authenticity(void) {
+#if ENABLE_SECURITY_SUBSYSTEM
+    // Здесь живет весь наш суровый код:
+    // Выдача синусоиды с ЦАП PA4, чтение АЦП PA5, проверка EEPROM и запуск карусели
+    if (is_counterfeit) {
+        activate_backlash_sabotage();
+    }
+#else
+    // В режиме отладки (0) этот блок пуст. 
+    // Плата всегда считается оригинальной, станок едет идеально со стопроцентной точностью.
+    is_counterfeit = false;
+#endif
+}
 
